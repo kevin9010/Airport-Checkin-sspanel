476add372c8ec49a503b8551e0571c7921c22541
kevin9010 <xufashu@qq.com>
2022-10-26 14:37:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ TG_USER_ID = os.environ.get('TGUSERID')
 
 def checkin(email=os.environ.get('EMAIL'), password=os.environ.get('PASSWORD'),
             base_url=os.environ.get('BASE_URL'), ):
+    print(email)
+    print(password)
     email = email.split('@')
     email = email[0] + '%40' + email[1]
     session = requests.session()
